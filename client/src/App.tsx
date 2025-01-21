ea871648e966c9388f72df95ae46e5dcc7adaf54
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2025-01-20 20:30:59 -0500

    fix up errors with comment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
@@ -25,14 +23,6 @@ function App() {
 
 
       <div className='main'>
-        {/*<div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>}
         {<h1>Lokr</h1>}
         <div className="card">
           <button onClick={() => setCount((count) => count + 1)}>
