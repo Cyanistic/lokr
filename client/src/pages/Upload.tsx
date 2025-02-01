e47c909c8afffb4af08f55cba83055f441d57be0
ReinaldoDemori <reinald.personal@gmail.com>
2025-01-31 20:27:10 -0500

    Uploading a file now works

--- client/src/pages/Upload.tsx
+++ client/src/pages/Upload.tsx
@@ -1,13 +1,26 @@
 import React, { useState } from 'react';
 
+interface FileMetadata {
+  name: string;
+}
+
 export default function Upload() {
 
     const [file, setFile] = useState<File | null>(null);
+    const [fileMeta, setFileMeta] = useState<FileMetadata | null>(null);
       
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const uploadedFile = e.target.files? e.target.files[0] : null;
         if (uploadedFile) {
           setFile(uploadedFile);
+
+          // Access basic metadata
+          const fileName = uploadedFile.name;
+
+          // Store the metadata in state
+          setFileMeta({
+            name: fileName,
+          });
         }
     };
       
@@ -19,8 +32,25 @@ export default function Upload() {
           console.log('No file selected');
           return;
         }
+
+        // Create a metadata object
+        const metadata = {
+          fileName: fileMeta?.name,
+          encryptedFileName: "c3RyaW5n",
+          encryptedKey: "c3RyaW5n",
+          encryptedMimeType: "c3RyaW5n",
+          isDirectory: false,
+          nonce: "c3RyaW5n",
+          parentId: null
+        };
+
+
         const formData = new FormData();
+        // Convert the metadata object to a JSON string
+        const metadataJSON = JSON.stringify(metadata);
+
         formData.append('file', file);
+        formData.append('metadata', metadataJSON);
 
         try {
             const response = await fetch('http://localhost:6969/api/upload', {
