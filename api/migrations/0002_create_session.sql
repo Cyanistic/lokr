4fbc4232ecff31a7b356a7a2edb1d1904d500c77
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2025-02-11 20:09:07 -0500

    fixed bug when calculating session expiration - session expiration was being calculated using raw addition instead of   proper built-in time functions, causing valid sessions to be   considered invalid - temporarily increased session idle duration for easier testing during   development

--- api/migrations/0002_create_session.sql
+++ api/migrations/0002_create_session.sql
@@ -1,7 +1,7 @@
 CREATE TABLE session (
     id BLOB PRIMARY KEY NOT NULL,
     user_id INTEGER NOT NULL,
-    idle_duration INTEGER NOT NULL DEFAULT 1800, -- Duration of inactivity in seconds before the session is invalidated (30 minutes)
+    idle_duration INTEGER NOT NULL DEFAULT 10800, -- Duration of inactivity in seconds before the session is invalidated (currently 3 hours, should be 30 minutes upon release)
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     last_used_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES user (id)
