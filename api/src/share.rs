69df322b712c9c784deab02409a53587546e0caf
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2025-02-13 01:47:30 -0500

    added retrieval of shared files on GET to `/api/shared` and misc refactor - files shared with the currently authorized user can now be retrieved   on GET to `/api/shared` using the same query params as `/api/file` - refactored code branches in get_file_metadata function to simplify   control flow and minimize duplicated code - added `hierarchify` function and `Hierarchify` trait to establish a consistent way   of transforming a row structure into a tree structure for representing   file hierarchies

--- api/src/share.rs
+++ api/src/share.rs
@@ -2,7 +2,7 @@ use std::time::Duration;
 
 use argon2::password_hash::{rand_core::OsRng, PasswordHasher, SaltString};
 use axum::{
-    extract::State,
+    extract::{Query, State},
     http::StatusCode,
     response::{IntoResponse, Response},
     Json,
@@ -17,7 +17,8 @@ use crate::{
     error::{AppError, ErrorResponse},
     state::AppState,
     success,
-    upload::is_owner,
+    upload::{is_owner, FileMetadata, FileQuery, UploadMetadata},
+    utils::Hierarchify,
     SuccessResponse,
 };
 
@@ -155,19 +156,159 @@ pub async fn share_with_user(
     owner_id: Uuid,
     receiver_id: Uuid,
 ) -> Result<(), AppError> {
+    if receiver_id == owner_id {
+        return Err(AppError::UserError((
+            StatusCode::BAD_REQUEST,
+            "Cannot share file with owner".into(),
+        )));
+    }
     if !is_owner(&state.pool, &owner_id, &file_id).await? {
         return Err(AppError::UserError((
             StatusCode::NOT_FOUND,
             "File not found".into(),
         )));
     }
-    sqlx::query!(
+    match sqlx::query!(
         "INSERT INTO share_user (file_id, user_id, encrypted_key) VALUES (?, ?, ?)",
         file_id,
         receiver_id,
         encrypted_key
     )
     .execute(&state.pool)
-    .await?;
+    .await
+    {
+        // If a FOREIGN KEY constraint is violated, it likely means that the parent id is invalid
+        // this is a user error and not a server error, so report it as such.
+        Err(e)
+            if e.as_database_error()
+                .and_then(|e| e.code())
+                .is_some_and(|code| code == "787") =>
+        {
+            return Err(AppError::UserError((
+                StatusCode::BAD_REQUEST,
+                "Invalid sharee id".into(),
+            )))
+        }
+        Err(e) => return Err(e.into()),
+        _ => {}
+    }
     Ok(())
+}
+
+#[utoipa::path(
+    get,
+    path = "/api/shared",
+    description = "Get files shared with the user",
+    params(FileQuery),
+    responses(
+        (status = OK, description = "File successfully retrieved", body = FileMetadata),
+        (status = BAD_REQUEST, description = "Invalid query params", body = ErrorResponse),
+        (status = NOT_FOUND, description = "File not found", body = ErrorResponse),
+    ),
+    security(
+        ("lokr_session_cookie" = [])
+    )
+)]
+pub async fn get_shared_file(
+    State(state): State<AppState>,
+    SessionAuth(user): SessionAuth,
+    Query(params): Query<FileQuery>,
+) -> Result<Response, AppError> {
+    let depth = params.depth.min(20);
+    let query = sqlx::query!(
+        r#"
+                WITH RECURSIVE children AS (
+                    -- Anchor member (root or specified node)
+                    SELECT
+                        0 AS depth,
+                        id,
+                        parent_id,
+                        encrypted_name,
+                        share_user.encrypted_key,
+                        nonce,
+                        owner_id,
+                        is_directory,
+                        mime,
+                        size,
+                        file.created_at,
+                        modified_at
+                    FROM file
+                    JOIN share_user ON file.id = share_user.file_id
+                    WHERE
+                        user_id = ? AND 
+                        id = COALESCE(?, id)
+                    UNION ALL
+
+                    -- Recursive member
+                    SELECT
+                        c.depth + 1,
+                        f.id,
+                        f.parent_id,
+                        f.encrypted_name,
+                        f.encrypted_key,
+                        f.nonce,
+                        f.owner_id,
+                        f.is_directory,
+                        f.mime,
+                        f.size,
+                        f.created_at,
+                        f.modified_at
+                    FROM file f
+                    JOIN children c ON f.parent_id = c.id
+                    WHERE
+                        c.depth < ?
+                    ORDER BY c.depth + 1
+                )
+                SELECT
+                    -- Goofy ahh workaround to get the query to work with sqlx
+                    depth AS "depth!: u32",
+                    id AS "id: Uuid",
+                    parent_id AS "parent_id: Uuid", 
+                    encrypted_name,
+                    encrypted_key,
+                    nonce,
+                    owner_id AS "owner_id: Uuid",
+                    is_directory,
+                    mime,
+                    size,
+                    created_at,
+                    modified_at
+                FROM (SELECT * FROM children ORDER BY depth LIMIT ? OFFSET ?) ORDER BY depth DESC
+    "#,
+        user.id,
+        params.id,
+        depth,
+        params.limit,
+        params.offset
+    )
+    .fetch_all(&state.pool)
+    .await?;
+
+    // Convert the query result into a tree structure
+    let root = query
+        .into_iter()
+        .map(|row| FileMetadata {
+            id: row.id,
+            created_at: row.created_at.and_utc(),
+            modified_at: row.modified_at.and_utc(),
+            owner_id: row.owner_id,
+            upload: UploadMetadata {
+                encrypted_file_name: row.encrypted_name,
+                encrypted_mime_type: row.mime,
+                encrypted_key: row.encrypted_key,
+                nonce: row.nonce,
+                is_directory: row.is_directory.is_some_and(|is_directory| is_directory),
+                parent_id: row.parent_id,
+            },
+            children: Vec::new(),
+        })
+        .hierarchify();
+    if params.id.is_some() && root.is_empty() {
+        Err(AppError::UserError((
+            StatusCode::NOT_FOUND,
+            "File not found".into(),
+        )))
+    } else {
+        Ok((StatusCode::OK, Json(root)).into_response())
+    }
 }
