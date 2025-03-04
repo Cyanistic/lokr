7209e5178922fab3e1213bdd028a45e4acd73706
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2025-03-04 01:20:15 -0500

    added protected routes, logout, and conditional root redirection - routes such as `/files` and `/profile` are now considered protected by   the frontend and will automatically redirect to the login page is the   user is not authenticated - a logout button is now displayed where the log in button would be if   the user is authenticated - the root page `/`, now redirects to `/home` if the user is not   authenticated and `/files` if the user is authenticated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,52 +1,69 @@
 import './App.css'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, Navigate, useNavigate } from 'react-router-dom'
 import Register from './pages/Register'
 import Profile from './pages/Profile'
 import FileExplorer from './pages/FileExplorer'
 import Login from './pages/Login'
 import Upload from './pages/Upload'
 import TestPage from "./pages/TestPage"
+import { isAuthenticated, logout } from "./utils.ts";
+import { ProtectedRoute } from './utils/ProtectedRoute.tsx'
 
 function App() {
 
   return (
     <>
-    <BrowserRouter>
+      <BrowserRouter>
 
-      <Navigation/>
+        <Navigation />
 
-      <Routes>
-      <Route path="/" element = {<Home />}/>
-      <Route path="/about" element = {<About />}/>
-      <Route path="/downloads" element = {<Downloads />}/>
-      <Route path="/login" element = {<Login />}/>
-      <Route path="/register" element = {<Register />}/>
-      <Route path="/profile" element = {<Profile/>}/>
-      <Route path="/files" element = {<FileExplorer/>}/>
-      <Route path="/test" element={<TestPage />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={
+            isAuthenticated() ? (< Navigate to="/files" replace />) : (<Navigate to="/home" replace />)
+          } />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/downloads" element={<Downloads />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/files" element={<FileExplorer />} />
+            <Route path="/test" element={<TestPage />} />
+          </Route>
+        </Routes>
 
-    </BrowserRouter>
+      </BrowserRouter>
     </>
   )
 }
 
 //Navigation Bar
-function Navigation(){
-  return(
+function Navigation() {
+  const navigate = useNavigate();
+  return (
     <div className='header'>
-        <Link to="/">Home</Link>
-        <Link to="/about">About Lokr</Link>
-        <Link to="/downloads">Downloads</Link>
+      <Link to="/home">Home</Link>
+      <Link to="/about">About Lokr</Link>
+      <Link to="/downloads">Downloads</Link>
+      {isAuthenticated() ? (
+        <Link to="/home" onClick={async (e) => {
+          e.preventDefault();
+          if (await logout()) {
+            navigate("/home");
+          }
+        }}>Logout</Link>
+      ) : (
         <Link to="/login">Log In</Link>
-      </div>
+      )}
+    </div>
   )
 }
 
 //Home Page
-function Home(){
+function Home() {
   return (
-    <div className = 'main'>
+    <div className='main'>
       <h1>Lokr</h1>
       <p>Encrypted File Sharing Website</p>
       <Upload />
@@ -58,7 +75,7 @@ function Home(){
 //About us page
 function About() {
   return (
-    <div className = 'main'>
+    <div className='main'>
       <h1>About Us Page</h1>
       <p>Placeholder for the about us page</p>
     </div>
@@ -67,14 +84,14 @@ function About() {
 
 //Downloads page
 function Downloads() {
-    return (
-      <div className = 'main'>
-        <h1>Download Page</h1>
-        <p>Placeholder for the Download page</p>
-      </div>
-    )
-  
-  }
+  return (
+    <div className='main'>
+      <h1>Download Page</h1>
+      <p>Placeholder for the Download page</p>
+    </div>
+  )
+
+}
 
 
 
