281019ab7393ac548a31e7ba9c11cd690649c39d
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2025-03-04 01:20:15 -0500

    added protected routes, logout, and conditional root redirection - routes such as `/files` and `/profile` are now considered protected by   the frontend and will automatically redirect to the login page is the   user is not authenticated - a logout button is now displayed where the log in button would be if   the user is authenticated - the root page `/`, now redirects to `/home` if the user is not   authenticated and `/files` if the user is authenticated

---
+++ client/src/utils/ProtectedRoute.tsx
@@ -0,0 +1,7 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { isAuthenticated } from "../utils";
+
+export function ProtectedRoute() {
+  const isAuth = isAuthenticated();
+  return isAuth ? <Outlet /> : <Navigate to="/login" replace />
+}
